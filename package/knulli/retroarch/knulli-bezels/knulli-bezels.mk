df7a493f3b56e48e4c80fa009f6670edfc066d7c
Chrizzo <chrizzo@unterderbruecke.de>
2025-05-27 22:41:41 +0200

    Updated default-knulli bezels

--- package/knulli/retroarch/knulli-bezels/knulli-bezels.mk
+++ package/knulli/retroarch/knulli-bezels/knulli-bezels.mk
@@ -3,13 +3,16 @@
 # knulli bezels
 #
 ################################################################################
-# Version.: Commits on May 24, 2025
-KNULLI_BEZELS_VERSION = 317e3ab307b207d6920cfb60db47348e321a1938
+# Version.: Commits on May 27, 2025
+KNULLI_BEZELS_VERSION = 14e46fdd2345648341a4d291ca5f903e223c41bb
 KNULLI_BEZELS_SITE = $(call github,chrizzo-hb,knulli-bezels,$(KNULLI_BEZELS_VERSION))
 
 define KNULLI_BEZELS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	rm -r $(TARGET_DIR)/usr/share/batocera/datainit/decorations/default-knulli
+  rm -r $(TARGET_DIR)/usr/share/batocera/datainit/decorations/default-knulli-sp
 	cp -rf $(@D)/default-knulli		      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -rf $(@D)/default-knulli-sp      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
 	(cd $(TARGET_DIR)/usr/share/batocera/datainit/decorations && ln -sf default-knulli default)
 
 endef
