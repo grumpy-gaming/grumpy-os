d7591bcddd4cc0d29fd3894a807d4ba86def20ce
Chrizzo <chrizzo@unterderbruecke.de>
2025-05-28 02:07:29 +0200

    No rm commands in packages

--- package/knulli/retroarch/knulli-bezels/knulli-bezels.mk
+++ package/knulli/retroarch/knulli-bezels/knulli-bezels.mk
@@ -9,8 +9,6 @@ KNULLI_BEZELS_SITE = $(call github,chrizzo-hb,knulli-bezels,$(KNULLI_BEZELS_VERS
 
 define KNULLI_BEZELS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/decorations
-	rm -r $(TARGET_DIR)/usr/share/batocera/datainit/decorations/default-knulli
-	rm -r $(TARGET_DIR)/usr/share/batocera/datainit/decorations/default-knulli-sp
 	cp -rf $(@D)/default-knulli		      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
 	cp -rf $(@D)/default-knulli-sp	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
 	(cd $(TARGET_DIR)/usr/share/batocera/datainit/decorations && ln -sf default-knulli default)
